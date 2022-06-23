3dfdadfdea49c7f9072e205d9a641c3a3e90a332
Alisa Pavlova <alipavlova2020@gmail.com>
2022-06-23 15:25:14 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 89;
+        int miles = amount / 20;
+        System.out.println("бонусные мили = " + miles);
+
+    }
+}
